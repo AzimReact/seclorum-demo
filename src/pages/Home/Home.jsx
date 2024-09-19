69e95db6cb12047f2338ba1281dbe604ea219d9d
AzimReact <azimreact@gmail.com>
2024-09-20 05:46:18 +0600

    ui bugfix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
+// import Layout from '../../components/Layout/Layout'
 import './home.css'
 
 function Home() {
   return (
-    <div className="homeFirstSection">
-      <h1 className='title'>Title of Home</h1>
-      <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia officiis maxime magnam adipisci ab eveniet aliquam? Quibusdam ullam aliquam dolor.</p>
-      <button className="btn">Связаться</button>
-    </div>
+    // <Layout>  
+      <div className="homeFirstSection">
+        <h1 className='title'>Title of Home</h1>
+        <p className='description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia officiis maxime magnam adipisci ab eveniet aliquam? Quibusdam ullam aliquam dolor.</p>
+        <button className="btn">Связаться</button>
+      </div>
+    // </Layout> 
   )
 }
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -8,21 +8,23 @@ function Projects() {
 
   return (
     <Layout>
-      <h1 className='project-title'>Проекты</h1>
-      <div className="projects-container">
-        {projects.map(project => (
-          <NavLink to={`/projects/${project.id}`} key={project.id} className="project-card">
-            <div
-              className="project-image"
-              style={{ backgroundImage: `url(${project.img})` }}
-            >
-              <div className="project-details">
-                <h2>{project.title}</h2>
-                <p>{project.description}</p>
+      <div>
+        <h1 className='project-title'>Проекты</h1>
+        <div className="projects-container">
+          {projects.map(project => (
+            <NavLink to={`/projects/${project.id}`} key={project.id} className="project-card">
+              <div
+                className="project-image"
+                style={{ backgroundImage: `url(${project.img})` }}
+              >
+                <div className="project-details">
+                  <h2>{project.title}</h2>
+                  <p>{project.description}</p>
+                </div>
               </div>
-            </div>
-          </NavLink>
-        ))}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </Layout>
   );
